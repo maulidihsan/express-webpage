16d5510e25f2750fa4069b321c27e36f2d8d3473
maulidihsan <maulid.ihsan@gmail.com>
2017-03-01 00:04:00 +0700

    first commit

---
+++ Server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/public/';
+
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+app.use(express.static(path));
+app.use("*",function(req,res){
+  res.sendFile(path + "index.html");
+});
+app.listen(8080,function(){
+  console.log("Live at Port 8080");
+});
